ea24cee417be5f081ca2a6f7c5548acaf6418b5c
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2025-04-14 14:53:57 -0700

    First Commit - Pushing to Repo

---
+++ src/test/java/com/example/task_api/TaskApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
